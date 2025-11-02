d5dd0971f22d7a8109ec550050047973042655fb
Joe7700487 <w0500154@nscc.ca>
2025-11-01 23:56:00 -0300

    added dynamic scrolling

--- app/src/main/java/com/example/weatherapp/ui/screens/DailyForecast.kt
+++ app/src/main/java/com/example/weatherapp/ui/screens/DailyForecast.kt
@@ -5,156 +5,130 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.weatherapp.MainViewModel
 import com.example.weatherapp.R
+import com.example.weatherapp.models.ForecastDay
 import com.example.weatherapp.ui.theme.WeatherAppTheme
+import kotlin.math.absoluteValue
+import androidx.compose.ui.util.lerp
 
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun DailyForecast(mainViewModel: MainViewModel, modifier: Modifier = Modifier) {
     val weather by mainViewModel.weather.collectAsState()
-    val day1 = weather?.forecast?.forecastDay[0]
-    val day2 = weather?.forecast?.forecastDay[1]
-    val day3 = weather?.forecast?.forecastDay[2]
-    LazyColumn (
+//    val day1 = weather?.forecast?.forecastDay[0]
+//    val day2 = weather?.forecast?.forecastDay[1]
+//    val day3 = weather?.forecast?.forecastDay[2]
+    val days = weather?.forecast?.forecastDay ?: emptyList()
+
+    val state = rememberLazyListState()
+
+    BoxWithConstraints (
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        LazyRow (
+            state = state,
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            contentPadding = PaddingValues(horizontal = this.maxWidth / 2 - 80.dp)
+        ) {
+            itemsIndexed(days) {i, day ->
+                val scale by remember {
+                    derivedStateOf {
+                        val currentState = state.layoutInfo.visibleItemsInfo.firstOrNull { it.index == i } ?: return@derivedStateOf 0.8f
+
+                        val itemCenter = currentState.offset + currentState.size / 2
+                        val distance = (itemCenter - (state.layoutInfo.viewportEndOffset / 2)).absoluteValue
+                        val maxDistance = state.layoutInfo.viewportEndOffset / 2
+
+                        lerp(
+                            start = 1f,
+                            stop = 0.8f,
+                            fraction = (distance.toFloat() / maxDistance).coerceIn(0f, 1f)
+                        )
+                    }
+                }
+                ForecastDay(
+                    day = day,
+                    modifier = Modifier.graphicsLayer {
+                        scaleX = scale
+                        scaleY = scale
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ForecastDay(day: ForecastDay?, modifier: Modifier = Modifier) {
+    Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = modifier.fillMaxSize().padding(all = 1.dp)
+        modifier = modifier
+            .padding(16.dp)
+            .border(1.dp, MaterialTheme.colorScheme.primary)
+            .padding(8.dp)
+
     ) {
-        item {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier.padding(8.dp).border(1.dp, MaterialTheme.colorScheme.primary).padding(8.dp)
-
-            ) {
-                AsyncImage(
-                    model = "https:${day1?.day?.condition?.icon}",
-                    contentDescription = day1?.day?.condition?.icon.toString(),
-                    Modifier.size(128.dp)
-                )
-                Text(
-                    text = day1?.date.toString(),
-                    modifier = modifier
-                )
-                Text(
-                    text = day1?.day?.temperatureHigh.toString() + "° - " + day1?.day?.temperatureLow.toString() + "°",
-                    modifier = modifier
-                )
-                Text(
-                    text = day1?.day?.condition?.text.toString(),
-                    modifier = modifier
-                )
-                Text(
-                    text = day1?.day?.precipitationProbability.toString() + "% " + day1?.day?.precipitationAmount.toString() + "mm",
-                    modifier = modifier
-                )
-                Text(
-                    text = day1?.day?.windSpeed.toString() + "km/h - ",
-                    modifier = modifier
-                )
-//                Text(
-//                    text = "humidity " + day1?.humidity.toString() + "%",
-//                    modifier = modifier
-//                )
-            }
-        }
-
-        item {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier.padding(8.dp).border(1.dp, MaterialTheme.colorScheme.primary).padding(8.dp)
-
-            ) {
-                AsyncImage(
-                    model = "https:${day2?.day?.condition?.icon}",
-                    contentDescription = day2?.day?.condition?.icon.toString(),
-                    Modifier.size(128.dp)
-                )
-                Text(
-                    text = day2?.date.toString(),
-                    modifier = modifier
-                )
-                Text(
-                    text = day2?.day?.temperatureHigh.toString() + "° - " + day2?.day?.temperatureLow.toString() + "°",
-                    modifier = modifier
-                )
-                Text(
-                    text = day2?.day?.condition?.text.toString(),
-                    modifier = modifier
-                )
-                Text(
-                    text = day2?.day?.precipitationProbability.toString() + "% " + day2?.day?.precipitationAmount.toString() + "mm",
-                    modifier = modifier
-                )
-                Text(
-                    text = day2?.day?.windSpeed.toString() + "km/h - ",
-                    modifier = modifier
-                )
-//                Text(
-//                    text = "humidity " + day1?.humidity.toString() + "%",
-//                    modifier = modifier
-//                )
-            }
-        }
-        item {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier.padding(8.dp).border(1.dp, MaterialTheme.colorScheme.primary).padding(8.dp)
-
-            ) {
-                AsyncImage(
-                    model = "https:${day3?.day?.condition?.icon}",
-                    contentDescription = day3?.day?.condition?.icon.toString(),
-                    Modifier.size(128.dp)
-                )
-                Text(
-                    text = day3?.date.toString(),
-                    modifier = modifier
-                )
-                Text(
-                    text = day3?.day?.temperatureHigh.toString() + "° - " + day3?.day?.temperatureLow.toString() + "°",
-                    modifier = modifier
-                )
-                Text(
-                    text = day3?.day?.condition?.text.toString(),
-                    modifier = modifier
-                )
-                Text(
-                    text = day3?.day?.precipitationProbability.toString() + "% " + day3?.day?.precipitationAmount.toString() + "mm",
-                    modifier = modifier
-                )
-                Text(
-                    text = day3?.day?.windSpeed.toString() + "km/h - ",
-                    modifier = modifier
-                )
-//                Text(
-//                    text = "humidity " + day1?.humidity.toString() + "%",
-//                    modifier = modifier
-//                )
-            }
-        }
+        AsyncImage(
+            model = "https:${day?.day?.condition?.icon}",
+            contentDescription = day?.day?.condition?.icon.toString(),
+            Modifier.size(128.dp)
+        )
+        Text(
+            text = day?.date.toString(),
+            fontSize = 25.sp
+        )
+        Text(
+            text = "High: " + day?.day?.temperatureHigh.toString(),
+            fontSize = 25.sp
+        )
+        Text(
+            text ="Low: " + day?.day?.temperatureLow.toString() + "°",
+            fontSize = 25.sp
+        )
+        Text(
+            text = day?.day?.condition?.text.toString(),
+            fontSize = 25.sp
+        )
+        Text(
+            text = day?.day?.precipitationProbability.toString() + "% rain, " + day?.day?.precipitationAmount.toString() + "mm",
+            fontSize = 25.sp
+        )
+        Text(
+            text = "Wind speed: " + day?.day?.windSpeed.toString() + "km/h",
+            fontSize = 25.sp
+        )
     }
 }
 
